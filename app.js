85acdeb15c7a6c72311d87ad07bd9520a9a0c461
Taylor Jones <taylor@taylorwjones.com>
2020-10-12 17:23:38 -0500

    Fixed using larger numbers than 9 on all operations and showing the larger numbers on the screen as well

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const decimal = document.querySelector(".decimal");
 let calcScreen = document.querySelector(".calculator-screen");
 let firstNum = "";
 let secondNum = "";
+let number;
 let operator = "";
 let operation = "";
+let input = "";
+let result;
 
 clearScreen.addEventListener("click", () => {
     calcScreen.value = 0;
@@ -18,19 +21,10 @@ clearScreen.addEventListener("click", () => {
 
 numBtn.forEach((button) => {
     button.addEventListener('click', () => {
-        let input = button.innerHTML;
-        if (firstNum === "" || secondNum === "") {
-            if (firstNum === "") {
-                firstNum = parseInt(input);
-                calcScreen.value = firstNum;
-            } else if (secondNum === "") {
-                secondNum = parseInt(input);
-                calcScreen.value = secondNum;
-            }
-        } else {
-            return "Error";
-        }
-    });
+            number = parseInt(button.innerHTML);
+            numberInput(number);
+            console.log(number);
+        });
 });
 
 operatorBtn.forEach((button) => {
@@ -38,18 +32,34 @@ operatorBtn.forEach((button) => {
         let input = button.innerHTML;
         operation = input;
         console.log(operation);
-    })
+    });
 });
 
 equalSign.addEventListener('click', () => {
     if (operation === "+") {
-        operate(operation, firstNum, secondNum);
+        add(parseInt(firstNum), parseInt(secondNum));
+        updateScreen(result);
+        firstNum = result;
+        secondNum = "";
+        operation = "";
     } else if (operation === "-") {
-        operate(operation, firstNum, secondNum);
+        subtract(parseInt(firstNum), parseInt(secondNum));
+        updateScreen(result);
+        firstNum = result;
+        secondNum = "";
+        operation = "";
     } else if (operation === "*") {
-        operate(operation, firstNum, secondNum);
+        multiply(parseInt(firstNum), parseInt(secondNum));
+        updateScreen(result);
+        firstNum = result;
+        secondNum = "";
+        operation = "";
     } else if (operation === "/") {
-        operate(operation, firstNum, secondNum);
+        divide(parseInt(firstNum), parseInt(secondNum));
+        updateScreen(result);
+        firstNum = result;
+        secondNum = "";
+        operation = "";
     } else {
         return "Error";
     }
@@ -58,40 +68,41 @@ equalSign.addEventListener('click', () => {
 //TODO: Need to implement adding a decimal point to the current number being entered
 decimal.addEventListener('click', () => {
     alert("Doesn't work yet");
-})
-
-// Operat function to take 2 numbers and what operation to do and return the answer
-function operate(sign, num1, num2) {
-    if (operation === "+") {
-        add(num1, num2);
-    } else if (operation === "-") {
-        subtract(num1, num2);
-    } else if (operation === "*") {
-        multiply(num1, num2);
-    } else if (operation === "/") {
-        divide(num1, num2);
-    }
-}
+});
 
 // Basic calculator functions
 function add(a, b) {
     result = a + b;
-    calcScreen.value = result;
-    firstNum = result;
-    secondNum = "";
 }
 function subtract(a, b) {
     result = a - b;
-    calcScreen.value = result;
-    firstNum = result;
-    secondNum = "";}
+}
 function multiply(a, b) {
     result = a * b;
-    calcScreen.value = result;
-    firstNum = result;
-    secondNum = "";}
+}
 function divide(a, b) {
     result = a / b;
-    calcScreen.value = result;
-    firstNum = result;
-    secondNum = "";}
+}
+
+
+function updateScreen(number) {
+    calcScreen.value = number;
+}
+
+function numberInput(number) {
+    if (operation === "") {
+        input = parseInt(number);
+        firstNum += parseInt(input);
+        updateScreen(firstNum);
+    } else {
+        input = parseInt(number);
+        secondNum += parseInt(input);
+        updateScreen(secondNum);
+    }
+}
+
+function resetScreen() {
+    calcScreen.value = 0;
+}
+
+//TODO: Add functionality to use numpad/num row to input numbers
