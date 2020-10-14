172be8af44e86b704810d9ddd03e77039bbc400c
Taylor Jones <taylor@taylorwjones.com>
2020-10-13 20:23:34 -0500

    Added keyboard, decimal, and backspace funtionality

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const operatorBtn = document.querySelectorAll(".operator");
+const backspace = document.querySelector(".backspace");
 const numBtn = document.querySelectorAll(".num-btn");
 const clearScreen = document.querySelector(".all-clear");
 const equalSign = document.querySelector(".equal-sign");
@@ -11,17 +12,24 @@ let operator = "";
 let operation = "";
 let input = "";
 let result;
+let answer;
+let newNum = "";
+let displayVal = 0;
+let decimalAdded = false;
+let secondDecimalAdded = false;
 
 clearScreen.addEventListener("click", () => {
     calcScreen.value = 0;
     firstNum = "";
     secondNum = "";
     operation = "";
+    decimalAdded = false;
+    secondDecimalAdded = false;
 })
 
 numBtn.forEach((button) => {
     button.addEventListener('click', () => {
-            number = parseInt(button.innerHTML);
+            number = button.innerHTML;
             numberInput(number);
             console.log(number);
         });
@@ -35,9 +43,25 @@ operatorBtn.forEach((button) => {
     });
 });
 
+decimal.addEventListener('click', () => {
+    if (secondNum === "") {
+        if (!decimalAdded) {
+            firstNum += ".";
+            decimalAdded = true;
+            updateScreen(firstNum);
+        }
+    } else {
+        if (!secondDecimalAdded) {
+        secondNum += ".";
+        secondDecimalAdded = true;
+        updateScreen(secondNum);
+        }
+    }
+});
+
 equalSign.addEventListener('click', () => {
     if (operation === "+") {
-        add(parseInt(firstNum), parseInt(secondNum));
+        add(parseFloat(firstNum), parseFloat(secondNum));
         updateScreen(result);
         firstNum = result;
         secondNum = "";
@@ -65,25 +89,28 @@ equalSign.addEventListener('click', () => {
     }
 });
 
-//TODO: Need to implement adding a decimal point to the current number being entered
-decimal.addEventListener('click', () => {
-    alert("Doesn't work yet");
-});
 
 // Basic calculator functions
 function add(a, b) {
-    result = a + b;
+    answer = a + b;
+    result = (Math.round(answer * 10) / 10).toFixed(1)
 }
 function subtract(a, b) {
-    result = a - b;
+    answer = a - b;
+    result = (Math.round(answer * 10) / 10).toFixed(1)
 }
 function multiply(a, b) {
-    result = a * b;
+    answer = a * b;
+    result = (Math.round(answer * 10) / 10).toFixed(1)
 }
 function divide(a, b) {
-    result = a / b;
+    if (b === 0 || b === "0"){
+        result = "No bad, no do";
+    } else{
+    answer = a / b;
+    result = (Math.round(answer * 10) / 10).toFixed(1)
+    }
 }
-
 
 function updateScreen(number) {
     calcScreen.value = number;
@@ -91,18 +118,115 @@ function updateScreen(number) {
 
 function numberInput(number) {
     if (operation === "") {
-        input = parseInt(number);
-        firstNum += parseInt(input);
-        updateScreen(firstNum);
+        input = number;
+        if (input === "DEL") {
+            firstNum = firstNum.substring(0, firstNum.length - 1);
+            updateScreen(firstNum);
+        } else {
+            firstNum += input;
+            updateScreen(firstNum);
+        }
     } else {
-        input = parseInt(number);
-        secondNum += parseInt(input);
-        updateScreen(secondNum);
+        input = number;
+        if (input === "DEL") {
+            secondNum = secondNum.substring(0, secondNum.length - 1);
+            updateScreen(secondNum);
+        }else {
+            secondNum += input;
+            updateScreen(secondNum);
+        }
     }
 }
 
-function resetScreen() {
-    calcScreen.value = 0;
-}
-
 //TODO: Add functionality to use numpad/num row to input numbers
+document.addEventListener("keydown", event => {
+    if (operation === "") {
+        if (event.key == "1") {
+            input = "1";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "2") {
+            input = "2";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "3") {
+            input = "3";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "4") {
+            input = "4";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "5") {
+            input = "5";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "6") {
+            input = "6";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "7") {
+            input = "7";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "8") {
+            input = "8";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "9") {
+            input = "9";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "0") {
+            input = "0";
+            firstNum += input;
+            updateScreen(firstNum);
+        }
+    } else {
+        if (event.key == "1") {
+            input = "1";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "2") {
+            input = "2";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "3") {
+            input = "3";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "4") {
+            input = "4";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "5") {
+            input = "5";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "6") {
+            input = "6";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "7") {
+            input = "7";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "8") {
+            input = "8";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "9") {
+            input = "9";
+            firstNum += input;
+            updateScreen(firstNum);
+        } else if (event.key == "0") {
+            input = "0";
+            firstNum += input;
+            updateScreen(firstNum);
+        }
+    }
+});
+//TODO: Change screen output to have if statements to determine to show an int or a float
+
+// Do the below as just stuff to improve on your coding with
+    // Refactor the functions for the operations to use switch cases
